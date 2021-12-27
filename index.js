c8a85bc70b4170a3e79999febf4c82dce12947a3
Miles Worthington <meworthington1@gmail.com>
2021-12-27 10:08:28 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let rideDistance;
+let city;
+let response;
+let tip;
+let charm;
+
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+  }
+  else if ((rideDistance >= 2000) && (rideDistance <= 2500)) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (rideDistance > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  city === "NYC" ? (response = "Ok, sounds good.") : (response = "No go.");
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+    case "not as generous":
+      charm = "Thank you.";
+      break;
+    default:
+      charm = "Bye.";
+      break;
+  }
+  return charm;
 }
